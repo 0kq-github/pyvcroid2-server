6087d5f54e9646f40c60ed723289ccbb8cd303d7
user.email <0kq@0kqnet.work>
2022-09-20 21:45:39 +0900

    commit

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # pyvcroid2-server
+
+pyvcroid2 + FastAPIのVOICEROID2用APIサーバー  
+/generateにリクエストを飛ばすと音声をwave形式で返します  
+
+## Usage
+VOICEROID2のbit数に合わせたpythonのインストール  
+```pip install -r requirements.txt```  を実行して必要なパッケージをインストール  
+VOICEROID2がインストールされたマシンでserver.pyを起動  
+ブラウザで`http://マシンのアドレス/docs`にアクセス
+
+## API エンドポイント
+
+### `GET /generate`
+|パラメーター|説明|型|
+|:-:|:-:|:-:|
+|text|読み上げ文字列|str|
+|speaker|話者|str|
+|mode|モード|str|
+|speed|読み上げ速度|float|
+|pitch|ピッチ|float|
+|volume|音量|float|
+|emphasis|抑揚|float|
+|pauseMiddle|短ポーズ時間|int|
+|pauseLong|長ポーズ時間|int|
+|pauseSentence|文末ポーズ時間|int|
+|masterVolume|マスター音量|float|
+
+#### Response
+|code|content-type|
+|:-:|:-:|
+|200|audio/wav|
+|500|text/plain|
+
+
+### `GET /speakers`
+
+#### Response
+|code|content-type|
+|:-:|:-:|
+|200|application/json|
+|500|text/plain|
+
---
+++ server.py
@@ -0,0 +1,55 @@
+import pyvcroid2
+from fastapi import FastAPI, Response
+from fastapi.responses import FileResponse
+import uvicorn
+import sys
+
+app = FastAPI()
+
+@app.get("/generate", response_class=FileResponse)
+def generate(
+  text:str, 
+  speaker:str, 
+  mode:str = "standard",
+  speed:float = 1,
+  pitch:float = 1, 
+  volume:float = 1,
+  emphasis:float = 1,
+  pauseMiddle:int = 100,
+  pauseLong:int = 100,
+  pauseSentence:int = 100,
+  masterVolume:float = 1
+  ):
+  with pyvcroid2.VcRoid2() as vc:
+    #話者の読み込み
+    vc.loadVoice(speaker)
+    vc.loadLanguage(mode)
+
+    #各種パラメーターの適用
+    vc.param.speed = speed
+    vc.param.pitch = pitch
+    vc.param.volume = volume
+    vc.param.emphasis = emphasis
+    vc.param.pauseMiddle = pauseMiddle
+    vc.param.pauseLong = pauseLong
+    vc.param.pauseSentence = pauseSentence
+    vc.param.masterVolume = masterVolume
+
+    #読み上げ不可な文字の除外
+    for i in text:
+      try:
+        i.encode("shift-jis")
+      except:
+        text = text.replace(i,"")
+
+    text = text.replace("\ufe0f","")
+    speech, _ = vc.textToSpeech(text)
+    return Response(content=speech,media_type="audio/wav")
+
+@app.get("/speakers",)
+def speakers():
+  with pyvcroid2.VcRoid2() as vc:
+    return vc.listVoices()
+
+if __name__ == "__main__":
+  uvicorn.run(app=app,host="0.0.0.0",port=int(sys.argv[1]))
